318c1415fe99a4303ee36d5f3e28f75f52bda33e
Igor Natan <igornatan4@gmail.com>
2023-04-28 16:34:22 -0300

    commit adicionando icone aba

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import PaginaInicial from "./paginas/PaginaInicial";
 import './styles/estilosGlobais.scss';
+import PaginaInicial from "./paginas/PaginaInicial";
 
 
 export default function App() {
