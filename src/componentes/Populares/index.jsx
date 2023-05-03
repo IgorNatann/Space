4fd40860f8368b2a0637d72c449ef3aff1f08b37
Igor Natan <igornatan4@gmail.com>
2023-05-03 17:24:32 -0300

    commit componente populares

--- src/componentes/Populares/index.jsx
+++ src/componentes/Populares/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import styles from './Populares.module.scss'
 import fotosPopulares from './fotos-populares.json'
 
 export default function Populares() {
   return (
-    <aside>
+    <aside className={styles.populares}>
         <h2>Populares</h2>
-        <ul>
-            
+        <ul className={styles.populares__imagens}>
+            {fotosPopulares.map((fotoPopular) => {
+              return(
+                <li key={fotoPopular.id}>
+                  <img src={fotoPopular.path} alt={fotoPopular.alt} />
+                </li>
+              )
+            })}
         </ul>
+        <button>Ver mais fotos</button>
     </aside>
     )
 }
--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -6,22 +6,22 @@ import styles from "./PaginaInicial.module.scss";
 
 import banner from "./banner.png"
 
-export default function PaginaInicial(){
+export default function PaginaInicial() {
     return (
         <>
-            <Cabecalho /> 
+            <Cabecalho />
             <main>
                 <section className={styles.principal}>
                     <Menu />
                     <div className={styles.principal__imagem}>
-                      <h1>A galeria mais completa do espaço</h1>
-                      <img src={banner} alt="A imagem da terra vista do espaço" />
+                        <h1>A galeria mais completa do espaço</h1>
+                        <img src={banner} alt="A imagem da terra vista do espaço" />
                     </div>
                 </section>
-            <div className={styles.galeria}>    
-            <Galeria />
-            <Populares />
-            </div>
+                <div className={styles.galeria}>
+                    <Galeria />
+                    <Populares />
+                </div>
             </main>
         </>
     )
