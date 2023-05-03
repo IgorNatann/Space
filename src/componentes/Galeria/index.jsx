65db4e61741019f55c895ce5b046948e2ba26cb6
Igor Natan <igornatan4@gmail.com>
2023-05-03 10:26:49 -0300

    commit component card das imagens

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
 import Tags from '../Tags'
 import styles from './Galeria.module.scss'
+import fotos from './fotos.json'
+import favorito from './favorito.png'
+import open from './open.png'
 
 export default function Galeria() {
   return (
-    <section>
+    <section className={styles.galeria}>
         <h2>Navegue pela galeria</h2>
         <Tags />
-        <ul>
+        <ul className={styles.galeria__cards}>
+          {fotos.map((foto)=> {
+            return(
+              <li key={foto.id} className={styles.galeria__card}>
+                <img
+                 className={styles.galeria__imagem} 
+                 src={foto.imagem}
+                 alt={foto.titulo}
+                />
+                <p className={styles.galeria__descricao}>{foto.titulo}</p>
+                <div>
+                  <p>{foto.creditos}</p>
+                  <span>
+                    <img src={favorito} alt="" />
+                    <img src={open} alt="" />
+                  </span>
+                </div>
+              </li>
+            )
+          })}
         </ul>
     </section>
   )
